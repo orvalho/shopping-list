4aef53fefe2e91a29c7aac88922f9fb9fbb65a5e
orvalho <rasa396@gmail.com>
2019-06-18 00:49:14 +0300

    Updates

---
+++ precache-manifest.235a05e27b2adfb560d6c575e29b7afc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01562fdc52cfa268fec53fa08682a6b7",
+    "url": "/shopping-list/index.html"
+  },
+  {
+    "revision": "68b9e6d8e23df29ae7c0",
+    "url": "/shopping-list/static/css/main.e90b6e8b.chunk.css"
+  },
+  {
+    "revision": "b6c2fd136df67fcb3eac",
+    "url": "/shopping-list/static/js/2.0fe01d1b.chunk.js"
+  },
+  {
+    "revision": "68b9e6d8e23df29ae7c0",
+    "url": "/shopping-list/static/js/main.62cf7e5e.chunk.js"
+  },
+  {
+    "revision": "f83e989e4d16bb609454",
+    "url": "/shopping-list/static/js/runtime~main.e244f075.js"
+  }
+]);
